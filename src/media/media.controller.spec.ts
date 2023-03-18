977b30c5fae5f5bc913961e7a8fc530bf501886f
ikechukwu-peter <pitiyke@gmail.com>
2023-03-18 18:40:53 +0100

    UPDATE: mock tests for media controller

--- src/media/media.controller.spec.ts
+++ src/media/media.controller.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { MediaController } from "./media.controller";
 import { MediaService } from "./media.service";
+import { OperationStatus } from "./types/media-response.types";
+import { PrismaService } from "./../prisma/prisma.service";
 
 describe("MediaController", () => {
   let controller: MediaController;
+  let mediaController: MediaController;
+  let mediaService: MediaService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +17,75 @@ describe("MediaController", () => {
     }).compile();
 
     controller = module.get<MediaController>(MediaController);
+
+    prisma = new PrismaService();
+    mediaService = new MediaService(prisma);
+    mediaController = new MediaController(mediaService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("getMediasBySearch", () => {
+    it("should return an array of media", async () => {
+      const result = [
+        {
+          id: "67eb6236-f78d-402f-a044-69b4d2909873",
+          type: "audio",
+          url: "https://gaudio.com",
+          status: "active",
+          title: "Sulaa",
+          description: "The souls music",
+          createdAt: "2023-03-18T15:13:00.176Z",
+          updatedAt: "2023-03-18T15:13:00.176Z",
+          deletedAt: null,
+        },
+      ];
+
+      const testData = {
+        status: OperationStatus.SUCCESS,
+        message: "Media retrieved successfully",
+        data: result,
+      };
+      jest
+        .spyOn(mediaService, "getMediasBySearch")
+        .mockImplementation(async () => testData);
+
+      expect(
+        await mediaController.getMediasBySearch({ query: "Sulaa" }),
+      ).toEqual(testData);
+    });
+  });
+
+  describe("getMediasPaginated", () => {
+    it("should return an array of media", async () => {
+      const result = [
+        {
+          id: "67eb6236-f78d-402f-a044-69b4d2909873",
+          type: "audio",
+          url: "https://gaudio.com",
+          status: "active",
+          title: "Sulaa",
+          description: "The souls music",
+          createdAt: "2023-03-18T15:13:00.176Z",
+          updatedAt: "2023-03-18T15:13:00.176Z",
+          deletedAt: null,
+        },
+      ];
+
+      const testData = {
+        status: OperationStatus.SUCCESS,
+        message: "Media retrieved successfully",
+        data: result,
+      };
+      jest
+        .spyOn(mediaService, "getMediasPaginated")
+        .mockImplementation(async () => testData);
+
+      expect(
+        await mediaController.getMediasPaginated({ page: 1, perPage: 2 }),
+      ).toEqual(testData);
+    });
   });
 });
