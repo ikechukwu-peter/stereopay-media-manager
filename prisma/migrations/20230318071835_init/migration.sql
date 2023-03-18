6ba7df58d47aa6aaba70a33b4346d2830b6bb5ce
ikechukwu-peter <pitiyke@gmail.com>
2023-03-18 08:21:30 +0100

    setup: prisma

---
+++ prisma/migrations/20230318071835_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Media` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` ENUM('audio', 'image') NOT NULL DEFAULT 'audio',
+    `url` VARCHAR(191) NOT NULL,
+    `status` ENUM('active', 'inactive') NOT NULL DEFAULT 'active',
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
