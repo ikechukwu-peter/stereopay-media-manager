d03d048ce0e3e9ee9ab9952771a04f7104392051
ikechukwu-peter <pitiyke@gmail.com>
2023-03-19 09:20:50 +0100

    UPDATE: modified http-exception filters to cater for class-validator error

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -1,23 +1,24 @@
 import {
-  ExceptionFilter, // Importing the ExceptionFilter interface from NestJS.
-  Catch, // Importing the Catch decorator from NestJS.
-  ArgumentsHost, // Importing the ArgumentsHost interface from NestJS.
-  Logger, // Importing the Logger class from NestJS.
-  HttpException, // Importing the HttpException class from NestJS.
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  Logger,
+  HttpException,
 } from "@nestjs/common";
-import { OperationStatus } from "./../media/types/media-response.types"; // Importing a custom enum.
-
+import { OperationStatus } from "./../media/types/media-response.types";
 @Catch(HttpException) // Decorating the class with the Catch decorator to indicate that it will handle HttpException.
 export class HttpExceptionFilter implements ExceptionFilter {
   // Implementing the ExceptionFilter interface.
-  private readonly logger = new Logger(); // Creating a logger instance.
+  private readonly logger = new Logger();
 
   catch(exception: HttpException, host: ArgumentsHost) {
     // Implementing the catch method from the ExceptionFilter interface.
     const ctx = host.switchToHttp(); // Getting the HTTP context from the ArgumentsHost.
     const response = ctx.getResponse(); // Getting the response object from the context.
     const status = exception.getStatus(); // Getting the HTTP status code from the HttpException.
-    const message = exception.message; // Getting the error message from the HttpException.
+    const message = Array.isArray(exception.getResponse()["message"])
+      ? exception.getResponse()["message"][0]
+      : exception.message; // Getting the error message from the HttpException.
     const error = exception.name; // Getting the error name from the HttpException.
 
     this.logger.log(`${JSON.stringify({ error, message, status })}`); // Logging the error details.
--- src/main.ts
+++ src/main.ts
@@ -16,20 +16,18 @@ async function bootstrap() {
   // enable cors for all domain
   app.enableCors();
 
-  // prisma hook
   const prismaService = app.get(PrismaService);
+  // This ensures that the application shuts down gracefully without any pending queries or transactions.
   await prismaService.enableShutdownHooks(app);
 
   // instantiate validation pipe globally
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // instantiate validation pipe globally
-  // only added to transform the response to the specified format rather than default nextjs format
+  // instantiate exception filter globally
+  // only added to transform the response to the specified format rather than default nestjs format
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const PORT = parseInt(configService.get("PORT")) ?? 4000;
-
-  console.log(PORT);
 
   await app.listen(PORT);
 }
